9b006ac1ed040b7444b4951d665ccb1ebf11d36a
VisionAsim <bscs23210@itu.edu.pk>
2024-09-08 15:21:03 +0500

    Final Commit

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import accountreducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
+import { configureStore } from "@reduxjs/toolkit";
+const store = configureStore({
+  reducer: {
+    account: accountreducer,
+    customer: customerReducer,
+  },
+});
+
+export default store;
